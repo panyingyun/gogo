639a4475052328a03ee17ece17f56b18f1061945
panyingyun <panyingyun@gmail.com>
2025-12-03 13:50:40 +0800

    update

--- golang-thirdpart/github-com-google-go-cmp-cmp/main.go
+++ golang-thirdpart/github-com-google-go-cmp-cmp/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"strings"
 	"time"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
+// 比较基础类型
 func CompareBasic() {
 	// 基本类型比较
 	x := 42
@@ -25,6 +28,7 @@ func CompareBasic() {
 	// 输出: "hello" != "world"
 }
 
+// 比较结构体
 type User struct {
 	ID        int
 	Name      string
@@ -48,7 +52,43 @@ func CompareStructIgnoreTimestamps() {
 	}
 }
 
+// 自定义比较函数
+
+type Product struct {
+	ID       string
+	Name     string
+	Price    float64
+	Category string
+}
+
+func CompareStructByCustomComparer() {
+	p1 := Product{ID: "P1", Name: "Laptop", Price: 999.99, Category: "Electronics"}
+	p2 := Product{ID: "P1", Name: "Laptop", Price: 999.991, Category: "electronics"}
+
+	opts := cmp.Options{
+		// 忽略大小写比较分类
+		cmp.Comparer(func(x, y string) bool {
+			return strings.EqualFold(x, y)
+		}),
+
+		// 价格允许小误差
+		cmp.Comparer(func(x, y float64) bool {
+			return math.Abs(x-y) < 0.01
+		}),
+
+		// 忽略 ID 字段
+		cmpopts.IgnoreFields(Product{}, "ID"),
+	}
+
+	if !cmp.Equal(p1, p2, opts) {
+		fmt.Printf("Products differ:\n%s", cmp.Diff(p1, p2, opts))
+	} else {
+		fmt.Println("Products is equal.")
+	}
+}
+
 func main() {
 	CompareBasic()
 	CompareStructIgnoreTimestamps()
+	CompareStructByCustomComparer()
 }
